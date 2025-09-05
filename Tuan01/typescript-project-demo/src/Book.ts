7ecc46b9a9103234f0f378ca4d20347f80bebee1
tongquockhanh2510 <22677951.khanh@student.iuh.edu.vn>
2025-09-05 16:53:29 +0700

    Create a class Book with attributes title, author, year.

---
+++ Tuan01/typescript-project-demo/src/Book.ts
@@ -0,0 +1,11 @@
+export class Book {
+    title : String ;
+    author : String;
+    year : number;
+
+    constructor (title : String , author: String , year: number){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }    
+}
