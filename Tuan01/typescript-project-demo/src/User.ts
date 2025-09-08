3f7cc4c6d1a7e485100600dcf2a4f2853ed757e5
tongquockhanh2510 <22677951.khanh@student.iuh.edu.vn>
2025-09-08 23:23:29 +0700

    Create a class BankAccount with balance. Add methods deposit() and withdraw().

---
+++ Tuan01/typescript-project-demo/src/User.ts
@@ -0,0 +1,17 @@
+export class User{
+
+    private _name: string;
+    constructor(name : string){
+        this._name = name;
+    }
+    
+   get name() :string{
+    return this._name;
+   }
+    
+   
+   public set name(name : string) {
+    this._name = name;
+   }
+   
+}
