b3d51dfeef8ffe6a0d0f2f886d4c6407c96a1221
tongquockhanh2510 <22677951.khanh@student.iuh.edu.vn>
2025-09-09 16:25:03 +0700

    8. Create a Product class with name, price. Create an array of products and filter products with

--- Tuan01/typescript-project-demo/dist/index.js
+++ Tuan01/typescript-project-demo/dist/index.js
@@ -3,20 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function hello(name) {
     return `Hello, ${name}!`;
 }
-const Car_1 = require("./Car");
-const Person_1 = require("./Person");
-const Student_1 = require("./Student");
-const Rectangle_1 = require("./Rectangle");
-const BankAccount_1 = require("./BankAccount");
+const Product_1 = require("./Product");
 console.log(hello("200Lab"));
-const person1 = new Person_1.Person("Khanh", 18);
-person1.displayInfo();
-const student = new Student_1.Student("khanh", 12, 12);
-student.displayInfo();
-const car = new Car_1.Car("Toyota", "mored", 12);
-car.showCarInfor();
-const retangle = new Rectangle_1.Retangle(12, 12);
-console.log("dien tich :" + retangle.area() + "Chu vi :" + retangle.perimeter());
-const bankAccount = new BankAccount_1.BankAccount(12);
-console.log("tiền gửi là :" + bankAccount.deposit(12));
-console.log("tiền rút là :" + bankAccount.withdraw(12));
+//  const person1 = new Person("Khanh",18);
+//  person1.displayInfo();
+//  const student = new Student( "khanh",12 ,12);
+//  student.displayInfo();
+//  const car = new Car("Toyota", "mored", 12)
+//  car.showCarInfor();
+//  const retangle = new Retangle(12,12)
+//  console.log("dien tich :"+ retangle.area() + "Chu vi :"+ retangle.perimeter())
+//  const bankAccount = new BankAccount(12);
+// console.log("tiền gửi là :"+bankAccount.deposit(12))
+// console.log("tiền rút là :"+ bankAccount.withdraw(12))
+const products = [
+    new Product_1.Product("Laptop", 1500),
+    new Product_1.Product("xe dap", 12300),
+    new Product_1.Product("dien thoai", 12)
+];
+const filteredProducts = products.filter(product => product.price > 100);
+console.log("San pham co gia >100");
+filteredProducts.forEach( product => console.log(product));
--- Tuan01/typescript-project-demo/src/index.ts
+++ Tuan01/typescript-project-demo/src/index.ts
@@ -7,20 +7,30 @@ import { Person } from "./Person";
 import { Student } from "./Student";
 import { Retangle } from "./Rectangle";
 import { BankAccount } from "./BankAccount";
+import { Product } from "./Product";
 console.log(hello("200Lab"));
- const person1 = new Person("Khanh",18);
- person1.displayInfo();
+//  const person1 = new Person("Khanh",18);
+//  person1.displayInfo();
 
- const student = new Student( "khanh",12 ,12);
- student.displayInfo();
+//  const student = new Student( "khanh",12 ,12);
+//  student.displayInfo();
 
- const car = new Car("Toyota", "mored", 12)
- car.showCarInfor();
+//  const car = new Car("Toyota", "mored", 12)
+//  car.showCarInfor();
 
- const retangle = new Retangle(12,12)
- console.log("dien tich :"+ retangle.area() + "Chu vi :"+ retangle.perimeter())
+//  const retangle = new Retangle(12,12)
+//  console.log("dien tich :"+ retangle.area() + "Chu vi :"+ retangle.perimeter())
 
- const bankAccount = new BankAccount(12);
+//  const bankAccount = new BankAccount(12);
 
-console.log("tiền gửi là :"+bankAccount.deposit(12))
-console.log("tiền rút là :"+ bankAccount.withdraw(12))
+// console.log("tiền gửi là :"+bankAccount.deposit(12))
+// console.log("tiền rút là :"+ bankAccount.withdraw(12))
+
+const products : Product[] =[
+  new Product("Laptop", 1500),
+  new Product("xe dap", 12300),
+  new Product("dien thoai", 12)
+];
+const filteredProducts = products.filter(product => product.price>100)
+console.log("San pham co gia >100")
+filteredProducts.forEach(console.log)
